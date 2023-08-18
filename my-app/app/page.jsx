fb53f6621c90fdb2cec9853dbe65ee499b33ae58
lucas271102 <lucas001spe@gmail.com>
2023-08-18 17:46:49 -0300

    next

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -1,8 +1,14 @@
+import { Button } from '@nextui-org/button'
 import React from 'react'
 
 function  HomePage() {
   return (
+    <>
+    
+    
     <div>Home</div>
+    <Button>Click me </Button>
+    </>
   )
 }
 
