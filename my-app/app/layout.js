233f47089271711e3dae7a1f8a6a17f391eab98e
lucas271102 <lucas001spe@gmail.com>
2023-08-20 21:12:08 -0300

    next

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -1,25 +1,28 @@
 import './globals.css'
 import { Providers } from './providers';
-import {IBM_Plex_Sans} from 'next/font/google'
+import {DM_Sans as roboto} from 'next/font/google'
+import  {Button}  from '@nextui-org/button';
 export const metadata = {
   title:'Car Room | Home',
   description :'Home page.',
   keywords:'Sitio web de autos, Car website'
 };
- const roboto =IBM_Plex_Sans({
-  weight:['400','500'],
+ const font =roboto({
+  weight:['400'],
   styles:['italic', 'normal'],
   subsets:['latin'],
 })
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className='dark'>
-      <body className={roboto.className}>
+    <html lang="en" className='light'>
+      <body className={font.className}>
         <Providers>
 
-        <nav>
-          <h1 className='bg-slate-300 text-black p-6 '>Navbar</h1>
+        <nav className='bg-white drop-shadow-lg border rounded-sm flex gap-16 text-black p-6 text-3xl font-light '>
+          <h1>Car Room</h1>
+          <Button>Cotizar</Button>
+          <Button>Comprar</Button>
         </nav>
 
         {children}
