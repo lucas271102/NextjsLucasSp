1cadeba7ab084c884e8d4be2adc3003feac83ad3
lucas271102 <lucas001spe@gmail.com>
2023-08-22 15:55:03 -0300

    practicando nextJs

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -1,8 +1,21 @@
+import PostCards from "@/components/PostCards";
 import { Button } from "@nextui-org/button";
 import Link from "next/link";
 import React from "react";
 
-function HomePage() {
+
+
+/*Funcion para realizar peticiones del lado del servidor con next   */
+
+async function Homeposts(){
+  const res = await fetch('https://jsonplaceholder.typicode.com/photos?limit=50')
+  const data = await res.json();
+  return data 
+}
+
+   async function HomePage() {
+  const cars = await Homeposts()
+  console.log(cars)
   return (
     <>
       <div className="h-screen">Home
@@ -11,6 +24,11 @@ function HomePage() {
       <Link href="/about">
         <Button>Click me </Button>
       </Link>
+      <div>
+        {cars.map((car)=>(
+        <PostCards  car={car} key={car.id}/>
+        ))}
+      </div>
       </div>
     </>
   );
---
+++ my-app/components/PostCards.jsx
@@ -0,0 +1,22 @@
+
+'use client'
+import { Button } from '@nextui-org/button'
+import React from 'react'
+
+const PostCards = ({car}) => {
+  return (
+    <div className='flex'>
+
+<div key={car.id} >
+             <h3>{car.title}</h3>
+             <img src={car.url} className='w-24 h-24 flex'/>
+             <button onClick={()=>{
+                alert('Boton andando.')
+             }}>Clic</button>
+          </div>
+
+    </div>
+  )
+}
+
+export default PostCards
